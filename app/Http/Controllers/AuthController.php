21bec7a7eae39e231844a133ef06b4d7b48ca7f7
Marcelo Rocha Ferreira <marcelo@cpd.pmvc.ba.gov.br>
2020-10-02 11:38:11 -0300

    Criação de usuário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['create', 'login']]);
+    }
+
+    public function create(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(!$validator->fails()) {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $password = $request->input('password');
+
+            $emailExists = User::where('email', $email)->count();
+            if($emailExists === 0) {
+
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $newUser = new User();
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->save();
+
+            } else {
+                $array['error'] = 'E-mail já cadastrado.';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Dados incorretos.';
+            return $array;
+        }
+
+        return $array;
+    }
 }
