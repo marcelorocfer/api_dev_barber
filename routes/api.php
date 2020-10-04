0c9393a9c01eef629271ada0925f01857782be04
Marcelo Rocha Ferreira <marceloengecomp@gmail.com>
2020-10-04 17:53:25 -0300

    Implementação da resposta 401 Unauthorized.

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,8 @@ Route::get('/ping', function() {
 });
 */
 
+Route::get('/401', 'AuthController@unauthorized')->name('login');
+
 Route::post('/auth/login', 'AuthController@login');
 Route::post('/auth/logout', 'AuthController@logout');
 Route::post('/auth/refresh', 'AuthController@refresh');
