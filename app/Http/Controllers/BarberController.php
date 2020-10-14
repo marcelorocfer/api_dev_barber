fc7d2a64a4b20c12560449239f13a147ca2d13e9
Marcelo Rocha Ferreira <marcelo@cpd.pmvc.ba.gov.br>
2020-10-14 11:44:24 -0300

    Buscando os barbeiros

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -267,6 +267,29 @@ class BarberController extends Controller
         return $array;
     }
 
+    public function search(Request $request)
+    {
+        $array = ['error' => '', 'list' => []];
+
+        $q = $request->input('q');
+
+        if($q) {
+            $barbers = Barber::select()
+                ->where('name', 'LIKE', '%'.$q.'%')
+                ->get();
+
+            foreach($barbers as $bkey => $barber) {
+                $barbers[$bkey]['avatar'] = url('media/avatars'.$barbers[$bkey]['avatar']);
+            }
+
+            $array['list'] = $barbers;
+        } else {
+            $array['error'] = 'Digite algo para a sua busca.';
+        }
+
+        return $array;
+    }
+
 
 
     /*
